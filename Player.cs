be82ebfaa4150b60c62286a4aa9768c7388f3640
chrisloog <122225702+chrisloog@users.noreply.github.com>
2023-09-16 14:56:35 +0300

    Update Player.cs

--- Player.cs
+++ Player.cs
@@ -1,16 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
 
     private int health = 10;
 
+    public AudioSource audioSource;
+    public AudioClip damageSound;
+
     public void TakeDamage(int damage)
     {
+
         health -= damage;
-        print("Player's health: " + health);
+
+        if (health > 0) {
+            audioSource.PlayOneShot(damageSound);
+        }
+        else {
+            int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(sceneIndex);
+        }
     }
 
 }
