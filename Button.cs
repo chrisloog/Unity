1f42443e6f12c28a39a250be8398c3d699008f1e
chrisloog <122225702+chrisloog@users.noreply.github.com>
2023-10-14 14:52:05 +0300

    Add files via upload

---
+++ Button.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    public Animator button;
+    public Animator toOpen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        button.enabled = true;
+        toOpen.enabled = true;
+    }
+}
