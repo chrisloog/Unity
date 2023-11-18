19ddeccba4ec41353642fe96d1f08750a468cc1c
chrisloog <122225702+chrisloog@users.noreply.github.com>
2023-11-18 16:41:02 +0200

    Add files via upload

---
+++ CharacterAnim.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnim : MonoBehaviour
+{
+    public Animator animator;
+    int isWalkingHash, isRunningHash;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isWalkingHash = Animator.StringToHash("isWalking");
+        isRunningHash = Animator.StringToHash("isRunning");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isRunning = animator.GetBool(isRunningHash);
+        bool isWalking = animator.GetBool(isWalkingHash);
+        bool forwardPressed = Input.GetKey("w");
+        bool runPressed = Input.GetKey("left shift");
+
+        if (isWalking && forwardPressed)
+        {
+            animator.SetBool("isWalking", true);
+        }
+
+        if (isWalking && !forwardPressed)
+        {
+            animator.SetBool("isWalking", false);
+        }
+
+        if(!isRunning && (forwardPressed && runPressed))
+        {
+            animator.SetBool("isRunning", true);
+        }
+
+        if(isRunning && (forwardPressed || runPressed))
+        {
+            animator.SetBool("isRunning", false);
+        }
+    }
+}
