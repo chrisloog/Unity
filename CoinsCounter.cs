e5b8a15b7aef52e46ab368155ca2b79f1af9224b
chrisloog <122225702+chrisloog@users.noreply.github.com>
2024-07-04 15:46:41 +0300

    Add files via upload

---
+++ CoinsCounter.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinsCounter : MonoBehaviour
+{
+    public int coins;
+
+    public void CollectCoins()
+    {
+        coins++;
+
+        print("Collected coins: " + coins);
+    }
+ 
+}
